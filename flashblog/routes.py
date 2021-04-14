2fc87c2df8018e0e2cf80628227ed6257325914a
catakeat <arsulescucatalin@yahoo.com>
2021-04-14 12:57:38 +0300

    test de update

--- flashblog/routes.py
+++ flashblog/routes.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flaskblog import db, login_manager, app
 from flask_login import UserMixin
-
+#test
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
